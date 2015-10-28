025c189c9965491efa38edfeba1c6a6c61cd41ba
Miguel Vaz <migueljvaz@gmail.com>
2015-10-28 02:46:49 +0100

    added paper

--- kmeans_aux.py
+++ kmeans_aux.py
@@ -13,7 +13,7 @@ def generate_three_circles():
     return X, labels_true, centres
 
 def generate_two_moons():
-    [X, true_labels] = make_moons(n_samples=300, noise=.05)
+    [X, true_labels] = make_moons(n_samples=200, noise=.05)
     return X, true_labels
 
 
@@ -33,10 +33,19 @@ def compare_kmeans_with_prior(k_means_cluster_centres, kmeans_labels, centres_tr
     is_correct = order[kmeans_labels] == labels_true
     return distance, order, is_correct
 
-def create_datasource(X, labels, centres, palette='colorblind'):
+def create_datasource(X, labels, number_categories, palette='colorblind'):
     # prepare data source
     x,y = [list(t) for t in zip(*X)]
-    colors = pd.Series(list(sns.color_palette(palette, len(centres)).as_hex()))
+    colors = pd.Series(list(sns.color_palette(palette, number_categories).as_hex()))
     clr = colors[labels].values
     src = ColumnDataSource(dict(x=x, y=y, fill_color=clr, radius=[0.01]*len(x))) #, radius=.01, fill_color=clr))
     return src
+
+def create_adjacency_matrix(similarity, num_neighbours=5):
+    A = ( np.ones(similarity.shape) - np.eye(similarity.shape[0])) * similarity
+    for i in range(A.shape[0]):
+        s = np.sort(A[i])
+        s = s[::-1] #reverse order
+        A[i][A[i]<s[num_neighbours]] = 0
+    A = np.fmax(A, A.T)
+    return A
